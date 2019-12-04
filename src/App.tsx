5db5d82c40155bf0700517968f0115ea0e21cadc
HADMARINE <joonlee031@live.com>
2019-12-05 01:18:39 +0900

    Update App.tsx
    Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Home from "./pages/MainPage";
+import NotFound from "./pages/NotFound";
+
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
