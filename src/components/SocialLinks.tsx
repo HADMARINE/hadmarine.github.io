c2af8ad5ae8ae3788b138d9c013a762a319bd4f8
HADMARINE <joonlee031@live.com>
2019-12-05 17:44:28 +0900

    Update SocialLinks.tsx
    change link

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 import styled, { createGlobalStyle } from "styled-components";
 
 import Github from "../image/github.png";
-import Facebook from "../image/facebook.png";
+import Facebook from "../image/facebook.svg";
 import Kakaotalk from "../image/kakaotalk.png";
 import Telegram from "../image/telegram.png";
+import Mail from "../image/mail.png";
+import Phone from "../image/phone.png";
 
 const GlobalStyle = createGlobalStyle`
 
@@ -24,17 +26,23 @@ export default class SocialLinks extends Component {
     return (
       <div>
         <GlobalStyle />
-        <a href="http://git.hadmarine.com">
+        <a href="http://git.hadmarine.com" target="_blank">
           <img src={Github} alt="Github logo" />
         </a>
-        <a href="http://fb.hadmarine.com">
+        <a href="http://fb.hadmarine.com" target="_blank">
           <img src={Facebook} alt="Facebook logo" />
         </a>
-        <a href="http://fb.hadmarine.com">
-          <img src={Kakaotalk} alt="Facebook logo" />
+        <a href="https://open.kakao.com/o/sTY9SYNb" target="_blank">
+          <img src={Kakaotalk} alt="Kakaotalk logo" />
         </a>
-        <a href="https://t.me/HADMARINE">
+        <a href="https://t.me/HADMARINE" target="_blank">
           <img src={Telegram} alt="Facebook logo" />
+        </a>
+        <a href="mailto:contact@hadmarine.com" target="_blank">
+          <img src={Mail} alt="Mail logo" />
+        </a>
+        <a href="tel:+827046069485" target="_blank">
+          <img src={Phone} alt="Phone logo" />
         </a>
       </div>
     );
