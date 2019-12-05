a07fc93d0f2078067c0d4e3b51224f0ff7e39bf6
HADMARINE <joonlee031@live.com>
2019-12-05 16:46:46 +0900

    Update MainPage.tsx

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,20 +1,39 @@
 import React, { Component } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+
+import Headroom from "react-headroom";
+
+//STYLESHEET
 import "../styles/index.scss";
 
+//COMPONENTS
 import IntroductPhotoSection from "../components/IntroducePhotoSection";
+import BackPhoto from "../components/BackgroundImg";
+import SocialLinks from "../components/SocialLinks";
+
+//IMAGE
+import backImg from "../image/back.png";
+import profileImg from "../image/profile.png";
 
 export default class MainPage extends Component {
   render() {
     return (
-      <div className="Style-center" style={{ margin: "1rem" }}>
-        <IntroductPhotoSection />
-        <p
-          className="Text-xlarge Text-bold Font-quicksand"
-          style={{ letterSpacing: "2px", marginTop: "10rem" }}
-        >
-          Hello World!
-        </p>
+      <div>
         <Headroom>헤더</Headroom>
+        <div className="Style-center">
+          <BackPhoto src={backImg} margin={0} />
+          {/* <IntroductPhotoSection /> */}
+          <p className="Text-xlarge Text-bold" style={{ marginTop: "10rem" }}>
+            HADMARINE
+          </p>
+          <p className="Text-small">Self-directing Active Developer</p>
+          <div className="Blank-small" />
+          <SocialLinks />
+          <p className="Text"></p>
+
+          <BackPhoto src={profileImg} margin={100} />
+          <div className="Blank-large" />
+        </div>
       </div>
     );
   }
