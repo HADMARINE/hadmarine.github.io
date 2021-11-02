5722d0433cc068513821bb8fd9bbc292587f1c48
HADMARINE <contact@hadmarine.com>
2021-11-02 16:16:31 +0900

    refactor: delete unused HOC

--- src/components/useParallaxProvider.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ParallaxProvider } from 'react-scroll-parallax';
-
-const useParallaxProvider = (component: JSX.Element) => {
-  return <ParallaxProvider>{component}</ParallaxProvider>;
-};
-
-export default useParallaxProvider;
